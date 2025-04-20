a49fb6e98015dacd54a6d16d3b8d080a867bf2c1
Malik Khizer <malik.khizer@rapidcompute.com>
2025-04-20 13:27:35 +0500

    comment init

--- web_app/forms/__init__.py
+++ web_app/forms/__init__.py
@@ -1,6 +1,6 @@
-from web_app.forms.menus import *
-from web_app.forms.role import *
-from web_app.forms.auth import *
-from web_app.forms.user import *
-from web_app.forms.reset_password import *
-from web_app.forms.contact_us import *
+# from web_app.forms.menus import *
+# from web_app.forms.role import *
+# from web_app.forms.auth import *
+# from web_app.forms.user import *
+# from web_app.forms.reset_password import *
+# from web_app.forms.contact_us import *
